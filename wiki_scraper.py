3687cec59f279d26dda3b55ac50839201e2b461a
Joel Rudsberg <rudsberg@live.se>
2022-11-18 19:38:54 +0100

    Submission Milestone 2: clean up notebook top to bottom

--- wiki_scraper.py
+++
@@ -1,7 +0,0 @@
-import wikipedia
-
-# To get data from event, simply call wikipedia.summary with the title of the page 
-subjects = ["World War II", "September 11 attacks"]
-for subject in subjects:
-    summary = wikipedia.summary(subject)
-    print(summary)
